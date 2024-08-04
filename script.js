9dcf6a105bc82fad5f74ab07ef1fded1a518a229
timo9939 <timothylau20199@gmail.com>
2024-08-04 22:12:13 +0100

    bubble shape button created

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("js work")
+
+
+
+
+function calculateScore() {
+    let resultText;
+
+    if (score >= 10) {
+        resultText = "You are an Early Bird (Lark).";
+    } else if (score >= 7) {
+        resultText = "You are an Intermediate (Hummingbird).";
+    } else if (score >= 4) {
+        resultText = "You are a Biphasic Sleeper.";
+    } else {
+        resultText = "You are a Night Owl.";
+    }
+}
